5d7687f45078da25f2c5a497bf0aa4ecc7078d41
LordPinguim <jf.lipeto@gmail.com>
2022-10-01 12:14:01 -0400

    Inicio

---
+++ NotesCube/NotesCube/Controler /ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotesCube
+//
+//  Created by ICMMAC09-BF7F on 01/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
